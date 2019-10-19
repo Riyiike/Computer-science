ac16b4a479d20731c0ea43a408929a760d9590af
abidex4yemi <abidex4yemi@gmail.com>
2019-10-19 19:42:28 +0100

    Solve fibonacci using memoization for efficiency

--- Algocast/exercises/fib/index.js
+++ Algocast/exercises/fib/index.js
@@ -8,16 +8,51 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  const result = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
+/*Pure fibonacci without memoization(caching)*/
 
-    result.push(a + b);
+// function fib_v1(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
+// const fib = n => {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// module.exports = fib;
+
+const memoize = fn => {
+  const cache = {};
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+};
+
+let fib = n => {
+  if (n < 2) {
+    return n;
   }
 
-  return result[n];
-}
+  return fib(n - 1) + fib(n - 2);
+};
+
+fib = memoize(fib);
 
 module.exports = fib;
