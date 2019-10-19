c87cab18d7616deb737f1c95efbbfb9fdd9c14cd
abidex4yemi <abidex4yemi@gmail.com>
2019-10-19 16:52:56 +0100

    Solve vowels challenge using regex

--- Algocast/exercises/vowels/index.js
+++ Algocast/exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels_v1(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const sanitizedString = str.replace(/[^\w]/g).toLowerCase();
+  let totalVowels = 0;
+
+  for (let char of sanitizedString) {
+    if (vowels.includes(char)) {
+      totalVowels += 1;
+    }
+  }
+
+  return totalVowels;
+}
+
+const vowels = str => {
+  const totalVowels = str.match(/[aeiou]/gi);
+  return totalVowels ? totalVowels.length : 0;
+};
 
 module.exports = vowels;
