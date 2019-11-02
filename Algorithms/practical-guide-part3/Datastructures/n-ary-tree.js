7d0375e399d3977caf4e60fb36e1c92d85589473
abidex4yemi <abidex4yemi@gmail.com>
2019-11-02 15:30:06 +0100

    Implement n-ary tree

---
+++ Algorithms/practical-guide-part3/Datastructures/n-ary-tree.js
@@ -0,0 +1,25 @@
+class Tree {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  addChild(value) {
+    const child = new Tree(value);
+    this.children.push(child);
+
+    return child;
+  }
+}
+
+const myTree = new Tree(1);
+
+const branch1 = myTree.addChild(2);
+const branch2 = myTree.addChild(3);
+const branch3 = myTree.addChild(4);
+const branch4 = myTree.addChild(5);
+
+branch1.addChild(4);
+branch1.addChild(5);
+
+console.log(myTree);
