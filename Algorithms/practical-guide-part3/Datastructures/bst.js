c648653c07bddb3cd32e616b76667f9f32c16af7
abidex4yemi <abidex4yemi@gmail.com>
2019-11-02 18:45:32 +0100

    Complete bst version 1

--- Algorithms/practical-guide-part3/Datastructures/bst.js
+++ Algorithms/practical-guide-part3/Datastructures/bst.js
@@ -32,6 +32,24 @@ class Bst {
 
     return this;
   }
+
+  contains(data) {
+    if (data === this.data) {
+      return true;
+    }
+
+    if (data < this.data && this.left) {
+      // Note: the exclamation (!!) is to default left (null) to false if not exist
+      //   return !!this.left && this.left.contains(data);
+      return this.left.contains(data);
+    } else if (data > this.data && this.right) {
+      // Note: the exclamation (!!) is to default right (null) to false if not exist
+      //   return !!this.right && this.right.contains(data);
+      return this.right.contains(data);
+    }
+
+    return false;
+  }
 }
 
 // const newNode = new Node(1);
@@ -41,4 +59,6 @@ myBst.insert(8);
 myBst.insert(7);
 myBst.insert(11);
 myBst.insert(12);
+
+console.log(myBst.contains(20));
 console.log(myBst);
