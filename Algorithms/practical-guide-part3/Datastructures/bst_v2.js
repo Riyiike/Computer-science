60787a5a974f5980fec806bc10b3dd268fcef962
abidex4yemi <abidex4yemi@gmail.com>
2019-11-03 09:54:58 +0100

    Implement deptFirstposdtorder bst method

--- Algorithms/practical-guide-part3/Datastructures/bst_v2.js
+++ Algorithms/practical-guide-part3/Datastructures/bst_v2.js
@@ -46,19 +46,58 @@ class Bst {
   }
 
   deptFirstInOrderTraversal(cb) {
+    // When reaches leaf node
+    // Note: pattern is => last left leave node => self => right
     if (!this.left && !this.right) {
       return cb(this);
     }
 
+    // left
     if (this.left) {
       this.left.deptFirstInOrderTraversal(cb);
     }
 
+    // self
     cb(this);
 
+    // right
     if (this.right) {
       this.right.deptFirstInOrderTraversal(cb);
     }
+  }
+
+  deptFirstPreOrderTraversal(cb) {
+    // Note: pattern is => self => left => right
+
+    // self
+    cb(this);
+
+    // left
+    if (this.left) {
+      this.left.deptFirstPreOrderTraversal(cb);
+    }
+
+    // right
+    if (this.right) {
+      this.right.deptFirstPreOrderTraversal(cb);
+    }
+  }
+
+  deptFirstPostOrderTraversal(cb) {
+    // Note: pattern is => left => right => self
+
+    // left
+    if (this.left) {
+      this.left.deptFirstPostOrderTraversal(cb);
+    }
+
+    // right
+    if (this.right) {
+      this.right.deptFirstPostOrderTraversal(cb);
+    }
+
+    // self
+    cb(this);
   }
 }
 
@@ -72,6 +111,14 @@ myBst.insert(40);
 
 // console.log(myBst);
 
-myBst.deptFirstInOrderTraversal(currentNode => {
+// myBst.deptFirstInOrderTraversal(currentNode => {
+//   console.log(currentNode.data);
+// });
+
+// myBst.deptFirstPreOrderTraversal(currentNode => {
+//   console.log(currentNode.data);
+// });
+
+myBst.deptFirstPostOrderTraversal(currentNode => {
   console.log(currentNode.data);
 });
