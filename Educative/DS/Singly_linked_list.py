254c1144890c356f225e410be6ff041cb902f6f4
abidex4yemi <abidex4yemi@gmail.com>
2019-11-16 17:56:50 +0100

    implement swap value method

--- Educative/DS/Singly_linked_list.py
+++ Educative/DS/Singly_linked_list.py
@@ -74,6 +74,75 @@ class LinkedList:
 
         prev.next = current_node.next
 
+    def delete_node_at_pos(self, pos):
+        if self.head is None:
+            return
+
+        if pos == 0:
+            self.head = self.head.next
+            return
+
+        current_node = self.head
+        prev = None
+        counter = 0
+
+        while current_node:
+            if counter == pos:
+                prev.next = current_node.next
+                break
+            prev = current_node
+            current_node = current_node.next
+            counter += 1
+
+    def len_iterative(self):
+        current_node = self.head
+        count = 0
+
+        while current_node:
+            current_node = current_node.next
+            count += 1
+
+        return count
+
+    def len_cursive(self, current_node=None):
+        if current_node is None:
+            return 0
+        else:
+            return 1 + self.len_cursive(current_node.next)
+
+    def swap_nodes(self, value1, value2):
+        if value1 == value2:
+            return
+
+        prev_1 = None
+        curr_1 = self.head
+
+        while curr_1 and curr_1.data != value1:
+            prev_1 = curr_1
+            curr_1 = curr_1.next
+
+        prev_2 = None
+        curr_2 = self.head
+
+        while curr_2 and curr_2.data != value2:
+            prev_2 = curr_2
+            curr_2 = curr_2.next
+
+        if not curr_1 or not curr_2:
+            return
+
+        if prev_1:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
+
+        if prev_2:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_1
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
+
 
 my_list = LinkedList()
 my_list.append("A")
@@ -85,6 +154,11 @@ my_list.append("D")
 
 # my_list.insert_after_node(my_list.head.next, "K")
 
-my_list.delete_node("Z")
+# my_list.delete_node("Z")
+
+# my_list.delete_node_at_pos(1)
+# print(my_list.len_iterative())
+# print(my_list.len_cursive(my_list.head))
+my_list.swap_nodes(my_list.head.data, my_list.head.next.data)
 
 my_list.print_list()
